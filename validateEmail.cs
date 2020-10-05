fa18a8bc5610badf988d1f1fe52e8fd166588a0e
Vishal Garg <vishugarg10004@gmail.com>
2020-10-05 20:25:21 +0530

    added use case 1 for 1st mandatory condition

---
+++ validateEmail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ValidateEmail
+{
+    class validateEmail
+    {
+        string pattern = @"^([a-zA-Z]{3,})";
+        public bool IsValidMail(string sample)
+        {
+          return  Regex.IsMatch(sample, pattern);
+        }
+
+    }
+}
