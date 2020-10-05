a6377725bad297b0db218de238e314b048948905
Vishal Garg <vishugarg10004@gmail.com>
2020-10-05 21:03:52 +0530

    [Vishal] added 4th UC where optional condition 1 is added

--- validateEmail.cs
+++ validateEmail.cs
@@ -6,7 +6,8 @@ namespace ValidateEmail
 {
     class validateEmail
     {
-        string pattern = @"^([a-zA-Z]{3,})\@([a-zA-Z0-9])+\.(com|net)$";
+        string pattern = @"^([a-z]{1})([a-z0-9])*[_.+-]?([a-z0-9])+\@([a-zA-Z0-9])+\.(com|net)*$";
+        // ask doubt about vg@gmail.com, i want to make id of v@capgemini.com--> which is not happening because of + at last, i want to make + compulsory only if there is special character.
         public bool IsValidMail(string sample)
         {
           return  Regex.IsMatch(sample, pattern);
