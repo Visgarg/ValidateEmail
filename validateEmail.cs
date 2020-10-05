9a6c39b347a89738043c0e4061fe0324f6815923
Vishal Garg <vishugarg10004@gmail.com>
2020-10-05 21:05:20 +0530

    [Vishal] refactor code to correction

--- validateEmail.cs
+++ validateEmail.cs
@@ -6,7 +6,7 @@ namespace ValidateEmail
 {
     class validateEmail
     {
-        string pattern = @"^([a-z]{1})([a-z0-9])*[_.+-]?([a-z0-9])+\@([a-zA-Z0-9])+\.(com|net)*$";
+        string pattern = @"^([a-z]{1})([a-z0-9])*[_.+-]?([a-z0-9])+\@([a-zA-Z0-9])+\.(com|net)$";
         // ask doubt about vg@gmail.com, i want to make id of v@capgemini.com--> which is not happening because of + at last, i want to make + compulsory only if there is special character.
         public bool IsValidMail(string sample)
         {
