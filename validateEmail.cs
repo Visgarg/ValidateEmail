a48ab3fded504c6d70b3cd11306db22b7ef48a7a
Vishal Garg <vishugarg10004@gmail.com>
2020-10-05 20:26:21 +0530

    [Vishal] added 2nd Use case where @ is mandatory and 2nd mandatory condition

--- validateEmail.cs
+++ validateEmail.cs
@@ -6,7 +6,7 @@ namespace ValidateEmail
 {
     class validateEmail
     {
-        string pattern = @"^([a-zA-Z]{3,})";
+        string pattern = @"^([a-zA-Z]{3,})\@([a-zA-Z0-9])+\.()$";
         public bool IsValidMail(string sample)
         {
           return  Regex.IsMatch(sample, pattern);
